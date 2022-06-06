fd0d5f511168f34fd60e16b5bb9158d98aa96887
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-06-06 14:02:36 -0400

    fix typos

--- docs/make.jl
+++ docs/make.jl
@@ -1,19 +1,19 @@
-using Documenter, PoissonRandom
+using Documenter, QuasiMonteCarlo
 
 include("pages.jl")
 
 makedocs(
-    sitename="PoissonRandom.jl",
+    sitename="QuasiMonteCarlo.jl",
     authors="Chris Rackauckas",
     modules=[PoissonRandom],
     clean=true,doctest=false,
     format = Documenter.HTML(analytics = "UA-90474609-3",
                              assets = ["assets/favicon.ico"],
-                             canonical="https://poissonrandom.sciml.ai/stable/"),
+                             canonical="https://quasimontecarlo.sciml.ai/stable/"),
     pages=pages
 )
 
 deploydocs(
-   repo = "github.com/SciML/PoissonRandom.jl.git";
+   repo = "github.com/SciML/QuasiMonteCarlo.jl.git";
    push_preview = true
 )
