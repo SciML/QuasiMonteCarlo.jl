e97220480a60436131e72a12cbc8dbc8cc1efd1d
Leticia-maria <leticiapequeno30@gmail.com>
2022-07-04 09:03:02 -0300

    Try to test again

--- test/runtests.jl
+++ test/runtests.jl
@@ -126,10 +126,9 @@ end
     @test s[1, :]≈[0.5, 0.25, 0.75, 0.125, 0.625] rtol=1e-7
     @test s[2, :]≈[1 / 3, 2 / 3, 1 / 9, 4 / 9, 7 / 9] rtol=1e-7
     
-    sample = []
-    s = QuasiMonteCarlo.sample(n, lb, ub, LowDiscrepancySample([2, 3], true))
-    for i in 1:1000000 push!(sample, mean(s)) end
-    @test mean(sample) ≈ 0.5 rtol=1e-5
+    testsample = []
+    for i in 1:1000000 push!(testsample, mean(QuasiMonteCarlo.sample(n, lb, ub, LowDiscrepancySample([2, 3], true)))) end
+    @test mean(testsample) ≈ 0.5 rtol=1e-5
 end
 
 @testset "Distribution 1" begin
