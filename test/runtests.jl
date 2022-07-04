4619d1fb7c99ac549b224870552fb97769b598cd
Letícia Maria Pequeno Madureira <leticiapequeno30@gmail.com>
2022-07-04 08:49:46 -0300

    Add tests for CP rotations

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using QuasiMonteCarlo, Distributions, Test
+using QuasiMonteCarlo, Distributions, StatsBase
 using Test
 
 #1D
@@ -125,6 +125,11 @@ end
     @test size(s) == (d, n)
     @test s[1, :]≈[0.5, 0.25, 0.75, 0.125, 0.625] rtol=1e-7
     @test s[2, :]≈[1 / 3, 2 / 3, 1 / 9, 4 / 9, 7 / 9] rtol=1e-7
+    
+    sample = []
+    s = QuasiMonteCarlo.sample(n, lb, ub, LowDiscrepancySample([2, 3], true))
+    for i in 1:1000000 push!(sample, mean(s)) end
+    @test mean(sample) ≈ 0.5 rtol=1e-5
 end
 
 @testset "Distribution 1" begin
