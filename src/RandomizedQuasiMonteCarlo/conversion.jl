646b3ccfcc4d64bc72e1b5286403d8d2aebd756e
David Métivier <46794064+dmetivie@users.noreply.github.com>
2022-11-22 16:23:31 +0100

    scrambling methods

---
+++ src/RandomizedQuasiMonteCarlo/conversion.jl
@@ -0,0 +1,107 @@
+"""
+    bits2unif(bits::BitArray)
+Convert a vector of M bits in base 2 into a number y∈[0,1[.
+"""
+function bits2unif(bits::BitArray)
+    # Turn sequence of bits into a point in [0,1)
+    # First bits are highest order
+    y = 0 // 1
+    for j in lastindex(bits):-1:1
+        y = (y + bits[j]) // 2
+    end
+    y
+end
+
+"""
+    bits2unif(bits::AbstractArray{<:Integer}, b::Integer)
+Convert a vector of M "bits" in base b into a number y∈[0,1[.
+"""
+function bits2unif(bits::AbstractArray{<:Integer}, b::Integer)
+    # Turn sequence of bits into a point in [0,1)
+    # First bits are highest order
+    y = 0 // 1
+    for j in length(bits):-1:1
+        y = (y + bits[j]) // b
+    end
+    y
+end
+
+"""
+    unif2bits(x<:AbstractMatrix, b::Integer; M=32)
+Return the b-adic decomposition of all element y of an array y = ∑ₖ yₖ/bᵏ a number yₖ∈[0,1[ -> [y₁, ⋯, yₘ] 
+"""
+function unif2bits(x::AbstractArray, b::Integer; M = 32)
+    bits = zeros(Int, M, size(x)...)
+    unif2bits!(bits, x, b)
+    return bits
+end
+
+function unif2bits!(bits::AbstractArray, x::AbstractArray, b::Integer)
+    @assert all([size(bits, d + 1) == size(x, d) for d in ndims(x)]) "Bit array of size $(size(bits)) instead of (M, $(size(x)))"
+    for i in CartesianIndices(x)
+        unif2bits!(@view(bits[:, i]), x[i], b)
+    end
+end
+
+function unif2bits(x::AbstractArray; M = 32)
+    bits = BitArray(undef, M, size(x)...)
+    unif2bits!(bits, x, 2)
+    return bits
+end
+"""
+    unif2bits(y<:Real, b::Integer; M=32)
+Return the b-adic decomposition y = ∑ₖ yₖ/bᵏ a number y∈[0,1[ -> [y₁, ⋯, yₘ] 
+"""
+function unif2bits(y::Real, b::Integer; M = 32)
+    bits = zeros(Int, M)
+    unif2bits!(bits, y, b)
+    return bits
+end
+"""
+    unif2bits(y; M=32)
+Return the binary decomposition y = ∑ₖ yₖ/2ᵏ a number y∈[0,1[ -> [y₁, ⋯, yₘ] 
+"""
+function unif2bits(y; M = 32)
+    bits = BitArray(undef, M)
+    unif2bits!(bits, y, 2)
+    return bits
+end
+
+check_zero(a::Rational; kwargs...) = a == 0
+#! isequal(0.18518518518518515... -1/3^2-2/3^3 , 0) Should be true but is not! It fouls the binary expansion! Hence the isapprox with a hand tuned tolerence
+check_zero(a::AbstractFloat; atol = 3e-16) = isapprox(a, 0, atol = atol)
+
+function unif2bits!(bits::AbstractVector{<:Integer}, y, b::Integer; kwargs...)
+    for j in eachindex(bits), bb in (b - 1):-1:1
+        a = y - bb // b^j
+        if check_zero(a; kwargs...)
+            bits[j] = bb
+            bits[(j + 1):end] .= 0
+            break # it breaks from the nested loop (see here)[https://stackoverflow.com/questions/39796234/how-to-break-out-of-nested-for-loops-in-julia]
+        elseif a > 0
+            bits[j] = bb
+            y = a
+        else
+            bits[j] = 0
+        end
+    end
+end
+
+#? This seems faster than @evalpoly(b, $bit...)
+#?  bi = rand(0:2,32);
+#? @btime @evalpoly(3, $bi...)
+#?   500.515 ns (1 allocation: 272 bytes)
+#? @btime QuasiMonteCarlo.bits2int($bi, 3)
+#?   13.113 ns (0 allocations: 0 bytes)  
+"""
+    bits2int(bit::AbstractMatrix{<:Integer}, b::Integer)
+Convert a vector of M "bits" in base b into an integer.
+"""
+function bits2int(bit::AbstractVector, b::Integer)
+    m = length(bit)
+    y = 0
+    for k in m:-1:1
+        y = y * b + bit[k]
+    end
+    return y
+end
---
+++ src/RandomizedQuasiMonteCarlo/scrambling_base_b.jl
@@ -0,0 +1,162 @@
+""" 
+    nested_uniform_scramble(points::AbstractArray; M=32)
+Return a scrambled version of the `points`. 
+The scrambling method is Nested Uniform Scrambling which was introduced in Owen (1995).
+`M` is the number of bits used for each points. One needs `M ≥ log(base, n)`. 
+"""
+function nested_uniform_scramble(rng::AbstractRNG, points::AbstractArray, b::Integer;
+                                 M = 32)
+    unrandomized_bits = unif2bits(points, b, M = M)
+    indices = which_permutation(unrandomized_bits, b)
+    random_bits = similar(unrandomized_bits)
+    nested_uniform_scramble_bit!(rng, random_bits, unrandomized_bits, indices, b)
+    random_points = similar(points)
+    for i in CartesianIndices(random_points)
+        random_points[i] = bits2unif(@view(random_bits[:, i]), b)
+    end
+    return random_points
+end
+
+function nested_uniform_scramble(points::AbstractArray, b::Integer; M = 32)
+    nested_uniform_scramble(Random.default_rng(), points, b; M = M)
+end
+
+""" 
+    nested_uniform_scramble_bit!(rng::AbstractRNG, random_bits::AbstractArray{<:Integer, 3}, origin_bits::AbstractArray{<:Integer, 3}, indices::AbstractArray{T, 3} where {T <: Integer}, b::Integer)
+In place version of Nested Uniform Scrambling (for the bit array). This is faster to use this functions for multiple scrambling of the same array.
+"""
+function nested_uniform_scramble_bit!(rng::AbstractRNG,
+                                      random_bits::AbstractArray{<:Integer, 3},
+                                      origin_bits::AbstractArray{<:Integer, 3},
+                                      indices::AbstractArray{T, 3} where {T <: Integer},
+                                      b::Integer)
+    # in place nested uniform Scramble.
+    #
+    m, n, d = size(indices)
+    M = size(random_bits, 3)
+    @assert m≥1 "We need m ≥ 1" # m=0 causes awkward corner case below.
+
+    for s in 1:d
+        theperms = getpermset(rng, m, b)         # Permutations to apply to bits 1:m
+        for k in 1:m                             # Here is where we want m > 0 so the loop works ok
+            random_bits[k, :, s] = (origin_bits[k, :, s] + theperms[k, indices[k, :, s]]) .%
+                                   b   # permutation by adding a bit modulo b
+        end
+    end
+    if M > m     # Paste in random entries for bits after m'th one
+        random_bits[(m + 1):M, :, :] = rand(rng, 0:(b - 1), n * d * (M - m))
+    end
+end
+
+function getpermset(rng::AbstractRNG, m::Integer, b::Integer)
+    # Get b^(k-1) random binary permutations for k=1 ... m
+    # m will ordinarily be m when there are n=b^m points
+    #
+    y = zeros(Int, m, b^(m - 1))
+    for k in 1:m
+        nₖ = b^(k - 1)
+        y[k, 1:nₖ] = rand(rng, 0:(b - 1), nₖ)
+    end
+    return y
+end
+
+getpermset(m::Integer, b::Integer) = getpermset(Random.GLOBAL_RNG, m, b)
+
+"""
+    which_permutation(bits::AbstractArray{<:Integer,3}, b)
+This function is used in Nested Uniform Scramble. 
+It assigns for each points (in every dimensions) `m` number corresponding to its location on the slices 1, 1/b, 1/b², ..., 1/bᵐ⁻¹ of the axes [0,1[.
+This also can be used to verify some equidistribution prorepreties.
+Here we create the `indices` array `m`, and not `M`. Indeed `(t,m,d)-net` in base `b` are scrambled up to the `1/bᵐ` component. 
+Higher order components are just used i.i.d `Uₖ ∼ 𝐔({0:b-1})` in `nested_uniform_scramble_bit!`.
+"""
+function which_permutation(bits::AbstractArray, b)
+    n, d = size(bits)[2:end]
+    m = logi(b, n)
+
+    indices = zeros(Int, m, n, d)
+    for j in axes(bits, 3)
+        which_permutation!(@view(indices[:, :, j]), bits[:, :, j], b)
+    end
+    return indices
+end
+
+function which_permutation!(indices::AbstractMatrix{<:Integer},
+                            bits::AbstractMatrix{<:Integer}, b::Integer)
+    @assert size(indices)[2:end]==size(bits)[2:end] "You need size(indices) = $(size(indices)) equal size(bits) = $(size(bits))"
+    indices[1, :] .= 0 # same permutation for all observations i
+    for i in axes(indices, 2)                     # Here is where we want m > 0 so the loop works ok
+        for k in axes(indices, 1)[2:end]
+            indices[k, i] = bits2int(@view(bits[1:(k - 1), i]), b) # index of which perms to use at bit k for each i
+        end
+    end
+    indices .+= 1 # array indexing starts at 1
+end
+
+""" 
+    nested_uniform_scramble(points::AbstractArray, b::Integer; M=32)
+Return a scrambled version of the `points`. 
+The scrambling method is Linear Matrix Scrambling which was introduced in Matousek (1998).
+`M` is the number of bits used for each points. One need `M ≥ log(base, n)`. 
+"""
+function linear_matrix_scramble(rng::AbstractRNG, points::AbstractArray, b::Integer; M = 32)
+    unrandomized_bits = unif2bits(points, b, M = M)
+    random_bits = similar(unrandomized_bits)
+    linear_matrix_scramble_bit!(rng, random_bits, unrandomized_bits, b)
+    random_points = similar(points)
+    for i in CartesianIndices(random_points)
+        random_points[i] = bits2unif(@view(random_bits[:, i]), b)
+    end
+    return random_points
+end
+
+function linear_matrix_scramble(points::AbstractArray, b::Integer; M = 32)
+    linear_matrix_scramble(Random.default_rng(), points, b; M = M)
+end
+
+#? Weird it should be faster than nested uniform Scrambling but here it is not at all.-> look for other implementation and paper
+""" 
+    linear_matrix_scramble_bit!(rng::AbstractRNG, random_bits::AbstractArray{<:Integer, 3}, origin_bits::AbstractArray{<:Integer, 3}, b::Integer)
+In place version of Linear Matrix Scrambling (for the bit array). This is faster to use this functions for multiple scrambling of the same array.
+"""
+function linear_matrix_scramble_bit!(rng::AbstractRNG,
+                                     random_bits::AbstractArray{<:Integer, 3},
+                                     origin_bits::AbstractArray{<:Integer, 3}, b::Integer)
+    # https://statweb.stanford.edu/~owen/mc/ Chapter 17.6 around equation (17.15).
+    #
+    M, n, d = size(origin_bits)
+    m = logi(b, n)
+    @assert m≥1 "We need m ≥ 1" # m=0 causes awkward corner case below.  Caller handles that case specially.
+
+    for s in 1:d
+        # Permutations matrix and shift to apply to bits 1:m
+        matousek_M, matousek_C = getmatousek(rng, m, b)
+
+        # xₖ = (∑ₗ Mₖₗ aₗ + Cₖ) mod b where xₖ is the k element in base b
+        # matousek_M (m×m) * origin_bits (m×n) .+ matousek_C (m×1) 
+        random_bits[1:m, :, s] = (matousek_M * origin_bits[1:m, :, s] .+ matousek_C) .% b
+    end
+
+    # Paste in random entries for bits after m'th one
+    if M > m
+        random_bits[(m + 1):M, :, :] = rand(rng, Bool, n * d * (M - m))
+    end
+end
+
+""" 
+    getmatousek(rng::AbstractRNG, m::Integer, b::Integer)
+Genereate the Matousek linear scramble in base b for one of the d components
+It produces a m x m bit matrix matousek_M and a length m bit vector matousek_C
+"""
+function getmatousek(rng::AbstractRNG, m::Integer, b::Integer)
+    matousek_M = LowerTriangular(zeros(Int, m, m)) + Diagonal(rand(rng, 1:(b - 1), m)) # Mₖₖ ∼ U{1, ⋯, b-1}
+    matousek_C = rand(rng, 0:(b - 1), m)
+    for i in 2:m
+        for j in 1:(i - 1)
+            matousek_M[i, j] = rand(rng, 0:(b - 1))
+        end
+    end
+    matousek_M, matousek_C
+end
+
+getmatousek(m::Integer, b::Integer) = getmatousek(Random.GLOBAL_RNG, m, b)
