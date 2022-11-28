b22e1c57868bd74c9484491cd936e247c5726b64
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-11-28 18:17:58 +0100

    allow equality

--- test/runtests.jl
+++ test/runtests.jl
@@ -31,9 +31,10 @@ n = 5
 d = 1
 
 for sampler in [GridSample(0.1), UniformSample(), SobolSample(), LatinHypercubeSample(), LatticeRuleSample(), Cauchy(), Normal(0, 4), GoldenSample()]
+    @show sampler
     A = QuasiMonteCarlo.sample(n, lb, ub, sampler)
-    @test all(all(x .< ub) for x in eachcol(A))
-    @test all(all(x .> lb) for x in eachcol(A))
+    @test all(all(x .<= ub) for x in eachcol(A))
+    @test all(all(x .>= lb) for x in eachcol(A))
 end
 
 @testset "1D" begin
