5f6c2a0e37f28f249fd81ad2127316cb8ef63197
ArnoStrouwen <arno.strouwen@telenet.be>
2023-07-03 11:17:40 +0200

    stricter docs and update contributing

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,15 @@ include("pages.jl")
 makedocs(sitename = "QuasiMonteCarlo.jl",
     authors = "Chris Rackauckas",
     modules = [QuasiMonteCarlo],
-    clean = true, doctest = false,
+    clean = true, doctest = false, linkcheck = true,
+    strict = [
+        :doctest,
+        :linkcheck,
+        :parse_error,
+        :example_block,
+        # Other available options are
+        # :autodocs_block, :cross_references, :docs_block, :eval_block, :example_block, :footnote, :meta_block, :missing_docs, :setup_block
+    ],
     format = Documenter.HTML(analytics = "UA-90474609-3",
         assets = ["assets/favicon.ico"],
         canonical = "https://docs.sciml.ai/QuasiMonteCarlo/stable/"),
