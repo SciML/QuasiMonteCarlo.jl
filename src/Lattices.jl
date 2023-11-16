a8cc797811a6f570bb9c86f6febfccdb2b26cc95
Ashutosh Bharambe <a.bharambe123@gmail.com>
2023-11-16 17:06:11 +0530

    chore: update docstring for `GridSample`

--- src/Lattices.jl
+++ src/Lattices.jl
@@ -1,7 +1,7 @@
 """
     GridSample(R::RandomizationMethod = NoRand()) <: DeterministicSamplingAlgorithm
 
-A simple rectangular grid lattice.
+A simple rectangular grid lattice. Samples `n` random samples from a grid with `dx = (ub -lb)/n``
 
 In more than 2 dimensions, grids have worse discrepancy than simple Monte Carlo. As a
 result, they should almost never be used for multivariate integration; their use is as
